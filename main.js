45fee0302a68764c67aabf500943f7d0654aaa39
jasonliao70 <69883797+jasonliao70@users.noreply.github.com>
2021-05-24 13:55:28 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow,Menu,MenuItem,screen } = require('electron');
 const ipc = require('electron').ipcMain;
 const os = require('os');
 const {dialog} = require('electron');
@@ -11,28 +11,45 @@ const axios = require('axios');
 const { net } = require('electron');
 const dgram = require('dgram');
 const server = dgram.createSocket('udp4');
-var num = 0;
 var info = [];
-var flg=0;
 var totel=0;
 var HOST = require("ip").address();
-//const ipcinfo = JSON.parse('{"group":{"007650000001":{"devmac":"007650000001","devip":"192.168.100.1","devport":"80","ver":"1.27"}}}');
+
 function createWindow () {
-// Create the browser window.
-const win = new BrowserWindow({
-	width: 800,
-	height: 600,
-	webPreferences: {
-		nodeIntegration: true,
-		contextIsolation: false,
-	}
-})
+// Create the browser window.grouplist
+
+  var menu = Menu.buildFromTemplate(
+  [
+    {label: '🔍Scan',click(){ win.webContents.send('dispscan');}},
+    {label: '⏬Firmware',click(){win.webContents.send('dispupdate');}},
+    {label:'🛠️About',
+      submenu: [ 
+        {label:'Ver:1.0.0'},
+        {label:'🔧Debug', role:'toggleDevTools'},
+        {label:'❌Close', role:'quit'}
+        ]}     
+  ])
+
+  Menu.setApplicationMenu(menu); 
+  const display = screen.getPrimaryDisplay();
+  const dimensions = display.workAreaSize;
+  const win = new BrowserWindow({
+          width: parseInt(dimensions.width * 0.5),
+          height: parseInt(dimensions.height * 0.5),
+          minWidth: parseInt(260),
+          minHeight: parseInt(300),
+          maxWidth: dimensions.width,
+          maxHeight: dimensions.height,
+          backgroundColor: '#2e2c29',
+          icon: 'css/ipc1.png',
+          webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+          }
+  })
 
 // Load the index.html of the app.
-win.loadFile('index.html')
-
-// Open the DevTools.
-win.webContents.openDevTools()
+  win.loadFile('index.html')
 }
 
 // This method will be called when Electron has finished
@@ -64,7 +81,7 @@ if (BrowserWindow.getAllWindows().length === 0) {
 // put them in separate files and require them here.
 
 
-ipc.on('uploadfw', (event, arg,arg1) =>  {   
+ipc.on('uploadfw', (event,arg,arg1) =>  {   
       
   const boundaryKey = '----WebKitFormBoundaryWdFFCdVh1ngt8UKf';
   const user = 'admin';
@@ -73,7 +90,7 @@ ipc.on('uploadfw', (event, arg,arg1) =>  {
   const cgi = '/cgi-bin/upload.cgi';   
   const form = new FormData();
   const dd='v1.28_682';
-  form.append('filename', fs.createReadStream('./root_uImage'));
+  form.append('filename', fs.createReadStream('root_uImage'));
   totel++;
  
   let request = net.request({
@@ -89,20 +106,19 @@ ipc.on('uploadfw', (event, arg,arg1) =>  {
       callback(user, pw);
     })
   request.on('response', (response) => {
-  console.log(`STATUS: ${response.statusCode}`);
-  dialog.showMessageBoxSync({
-    type: 'info',
-    title: 'MessageBox',
-    message: host+' Upgrade '+dd+' firmware OK',
-    cancelId: 0
-  })  
-  console.log(`HEADERS: ${JSON.stringify(response.headers)}`);
-  response.on('data', (chunk) => {
-    console.log(`BODY: ${chunk}`);
-  })
-  response.on('error', (error) => {
+    console.log(`STATUS: ${response.statusCode}`);
+    dialog.showMessageBoxSync({
+      type: 'info',
+      title: 'MessageBox',
+      message: host+' Upgrade '+dd+' firmware OK'
+    })  
+    console.log(`HEADERS: ${JSON.stringify(response.headers)}`);
+    response.on('data', (chunk) => {
+      console.log(`BODY: ${chunk}`);
+    })
+    response.on('error', (error) => {
       console.log(`ERROR: ${JSON.stringify(error)}`);
-   })
+    })
   })
   
   form.pipe(request, { end: false });
@@ -110,12 +126,15 @@ ipc.on('uploadfw', (event, arg,arg1) =>  {
       console.log("end");  
       request.end('\r\n--' + boundaryKey + '--\r\n');        
   });
-    
+  console.log(totel);
   if(totel == arg1)
   {  
+    var times=61000 * arg1;
     setTimeout(function(){
-    event.reply('rescan');
-  }, 60000 * arg1);
+      totel = 0;
+      event.reply('rescan');
+    },times);
+    if(arg1 == 1){ totel = 0;}
   }
 });
 
@@ -163,22 +182,26 @@ ipc.on('UDPGO', (event, arg) =>  {
       client.close();
     });
   } 
-  sendMsg();
 
-  server.on('message', (msg, rinfo) => {
-    
-    
-   
+  sendMsg();
+  var udpmsg={};
+  setTimeout(function(){
+    event.reply('groupmsg',udpmsg);
+
+  }, 3000 );
+  server.on('message', (msg, rinfo) => {   
     if(`${msg}`.indexOf('90-76') == -1)
     {  
-      //console.log(`${msg}`);
-
-      info[num] = `${msg}`;
-      //console.log(info);
-      num++;
-     
-      event.reply('UDPGO-reply',`${msg}`);
-
+      info = `${msg}`.split(",");
+      var devinfo={};
+      devinfo.mac=info[2];
+      devinfo.ip=info[3];
+      devinfo.nm=info[4];
+      devinfo.gw=info[5];
+      devinfo.name=info[6];
+      devinfo.dhcp=info[7];
+      devinfo.ver=info[8];
+      udpmsg[devinfo.mac]=devinfo; 
     }
   });
 
